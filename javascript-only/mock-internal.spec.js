3c63ea97bfb9f69853706a9280e1e3a21fc4a8dc
Nicolas DUBIEN <github@dubien.org>
2024-01-26 08:21:41 +0000

    add example to mock internals

---
+++ javascript-only/mock-internal.spec.js
@@ -0,0 +1,21 @@
+import { jest } from "@jest/globals";
+
+jest.unstable_mockModule("./src/internal.js", () => ({
+  compute: jest.fn(),
+}));
+const InternalMock = await import("./src/internal.js");
+const { play } = await import("./src/use-internal.js");
+
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+test("mock internal", () => {
+  // Arrange
+  const { compute } = InternalMock;
+  compute.mockReturnValue("from test");
+
+  // Act / Assert
+  expect(play()).toBe("from test");
+  expect(compute).toHaveBeenCalledTimes(1);
+});
