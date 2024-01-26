796eba0aff73539ea747416ad7175d7d6de029e3
Nicolas DUBIEN <github@dubien.org>
2024-01-26 08:39:41 +0000

    describe what happens

--- javascript-only/mock-internal-noext.spec.js
+++ javascript-only/mock-internal-noext.spec.js
@@ -32,3 +32,14 @@ test("mock internal without any extension on unstable_mockModule", () => {
 // The file is preserved as is!
 // But "await import('./src/use-internal')" is not valid from Node point-of-view.
 // Even with "node --experimental-vm-modules file-name.js"
+
+// What happens is jest-resolve!
+// It defines the following mappings:
+// - ./internal -> /workspaces/jest-esm-mocks-experiments/javascript-only/src/internal.js
+// - ./src/use-internal -> /workspaces/jest-esm-mocks-experiments/javascript-only/src/use-internal.js
+
+// To confirm that you can:
+// - Open "node_modules/jest-resolve/build/defaultResolver.js"
+// - Look for "const defaultResolver = (path, options)"
+// - Add "console.log(path + " -> " + result)" right after "const result = (0, _resolve().sync)(pathToResolve, resolveOptions);"
+// - Run your tests again
