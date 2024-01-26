9fc687b987a7471e620888f9153af16bfeb40e0c
Nicolas DUBIEN <github@dubien.org>
2024-01-26 08:32:34 +0000

    digging a bit for noext case...

--- javascript-only/mock-internal-noext.spec.js
+++ javascript-only/mock-internal-noext.spec.js
@@ -19,3 +19,16 @@ test("mock internal without any extension on unstable_mockModule", () => {
   expect(play()).toBe("from test");
   expect(compute).toHaveBeenCalledTimes(1);
 });
+
+// What happens on this file?
+
+// Actually Jest implicitely relies on babel-jest.
+// If you modify it, you can track the transpiled version easily:
+// - Open "node_modules/babel-jest/build/index.js"
+// - Look for "async processAsync(sourceText, sourcePath, transformOptions)"
+// - Add "if (sourcePath.includes('mock-internal-noext.spec')) { console.log(code) }" right after "const {code, map} = transformResult"
+// - Run your tests again
+
+// The file is preserved as is!
+// But "await import('./src/use-internal')" is not valid from Node point-of-view.
+// Even with "node --experimental-vm-modules file-name.js"
